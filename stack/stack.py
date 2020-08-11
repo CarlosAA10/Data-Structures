852f60c381bb1ad57bdd02c7a5ca3d55072a8c8b
Carlos Alvarado <Carlosalvaradoarguello10@gmail.com>
2020-08-11 11:30:45 -0500

    missing queue implementation

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,80 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+        
+
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+
+        return self.size
+
 
     def push(self, value):
         pass
 
     def pop(self):
         pass
+
+
+# stack = Stack()
+
+# stack.push(4) # we have one item in our array
+# stack.push(3)
+# stack.push(5)
+# stack.push(43)
+
+# # we should have 4 items added to our storage array
+
+
+# print(stack.__len__(), 'the length should be 4')
+# print(stack.storage)
+
+
+# print(stack.pop())
+# print(stack.pop())
+
+# # we should have 2 items removed from our array, leaving us 
+# # with a length of 2 items 
+# print(stack.__len__(), 'it should be 2')
+# # print(stack.storage)
+
+# print(len(stack), 'length of stack')
+
+# stack class implemented with array
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+
+#         return self.size
+
+
+#     def push(self, value):
+#         self.storage.append(value) # we added an item to our array 
+#         self.size += 1 # each time we call push, increment the size by one
+
+#     def pop(self):
+
+#         if self.size == 0:
+#             return None
+
+#         self.size -= 1 # successfully decrease the size respectively 
+#         return self.storage.pop() # succesfully takes away from our array
+
+
+
+
